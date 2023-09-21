1213b2e37e4fb56fbcc4f5c3d2fb009a0597806a
Cory Foy <github@cornetdesign.com>
2023-09-20 21:01:23 -0400

    Adds unit type and updates role, color and type to be enums

--- Sources/SwiftTAK/Utilities/TAKConstants.swift
+++ Sources/SwiftTAK/Utilities/TAKConstants.swift
@@ -23,33 +23,9 @@ public struct TAKConstants {
     
     public static let UDP_BROADCAST_URL = "239.2.3.1"
     
-    public static let TEAM_COLORS = [
-        "Blue",
-        "Dark Blue",
-        "Brown",
-        "Cyan",
-        "Green",
-        "Dark Green",
-        "Magenta",
-        "Maroon",
-        "Orange",
-        "Purple",
-        "Red",
-        "Teal",
-        "White",
-        "Yellow"
-    ]
-    
-    public static let TEAM_ROLES = [
-        "Team Member",
-        "Team Lead",
-        "HQ",
-        "Sniper",
-        "Medic",
-        "Forward Observer",
-        "RTO",
-        "K9"
-    ]
+    public static let TEAM_COLORS = TeamColor.allCases
+    public static let TEAM_ROLES = TeamRole.allCases
+    public static let UNIT_TYPES = UnitType.allCases
     
     // Helper Functions
     public static func certificateSigningPath(clientUid: String, appVersion: String) -> String {
@@ -57,4 +33,38 @@ public struct TAKConstants {
             .replacingOccurrences(of: "$UID", with: clientUid)
             .replacingOccurrences(of: "$VERSION", with: appVersion)
     }
+}
+
+public enum TeamRole: String, CaseIterable {
+    case TeamMember = "Team Member"
+    case TeamLead = "Team Lead"
+    case HQ = "HQ"
+    case Sniper = "Sniper"
+    case Medic = "Medic"
+    case ForwardObserver = "Forward Observer"
+    case RTO = "RTO"
+    case K9 = "K9"
+}
+
+public enum TeamColor: String, CaseIterable {
+    case Blue = "Blue"
+    case DarkBlue = "Dark Blue"
+    case Brown = "Brown"
+    case Cyan = "Cyan"
+    case Green = "Green"
+    case DarkGreen = "Dark Green"
+    case Magenta = "Magenta"
+    case Maroon = "Maroon"
+    case Orange = "Orange"
+    case Purple = "Purple"
+    case Red = "Red"
+    case Teal = "Teal"
+    case White = "White"
+    case Yellow = "Yellow"
+}
+
+public enum UnitType: String, CaseIterable {
+    case Ground = "Ground"
+    case Air = "Air"
+    case Sea = "Sea"
 }
